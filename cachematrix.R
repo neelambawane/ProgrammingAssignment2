340559705786a47df12c30c4818d6b04077cb77f
Neelam Bawane <neelambawane@gmail.com>
2020-07-11 20:38:20 +0530

    1st commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## creates a special "matrix" caching its inverse.
+makeCacheMatrix <- function(x = numeric()) {
+  inv <- NULL
+  get <- function() x
+  getInverse <- function() {
+    if(length(x)%%sqrt(length(x))==0) { ## make sure matrix is square
+      inv <<- solve(x)
+    }
+  }
+  list(get = get, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix. The cachesolve should retrieve 
+## the inverse from the cache if already calculated
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  if(!is.atomic(x)){      # check if the given parameter is atomic
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+      message("getting cached data")
+      return(inv)
+    }     
+  } else {        # if not atomic then make it
+    message("getting the inverse-- no cached data found")
+    return(makeCacheMatrix(x)$getInverse())
+  } 
+  
 }
+
+## non-atomic example
+f <- makeCacheMatrix(matrix(1:4, 2, 2))
+print(cacheSolve(f))
+
+## atomic examples
+print(cacheSolve(matrix(1:4, 2, 2)))
